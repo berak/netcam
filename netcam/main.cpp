afb3b7b4100af7ff48d891a8022258e1987f17b4
berak <p4p4p4.web.de>
2012-03-14 14:28:00 +0100

    0003

--- netcam/main.cpp
+++ netcam/main.cpp
@@ -6,20 +6,33 @@ using namespace cv;
 #include "thread.h"
 
 
-struct CriticalSection : public CRITICAL_SECTION 
-{
-	CriticalSection()	{ InitializeCriticalSection(this); 	}	
-	~CriticalSection() 	{ DeleteCriticalSection (this);    	}
+#ifdef _WIN32
+	struct CriticalSection : public CRITICAL_SECTION 
+	{
+		CriticalSection()	{ InitializeCriticalSection(this); 	}	
+		~CriticalSection() 	{ DeleteCriticalSection (this);    	}
 
-	void lock()			{ EnterCriticalSection( this );		}
-	void unlock()		{ LeaveCriticalSection( this );		}
-} g_critter;
+		void lock()			{ EnterCriticalSection( this );		}
+		void unlock()		{ LeaveCriticalSection( this );		}
+	} g_critter;
+#else
+	#include <pthread.h>
+	#include <unistd.h>
+	class Mutex {
+		pthread_mutex_t mutex;
+	public:
+		Mutex()              { pthread_mutex_init(&(mutex),NULL); }
+		~Mutex()             { pthread_mutex_destroy(&(mutex)); }
+		inline void lock()   { pthread_mutex_lock(&(mutex)); }
+		inline void unlock() { pthread_mutex_unlock(&(mutex)); }
+	} g_critter;
+#endif
+
 
 
 void client(int s, Mat & frame)
 {
-	Birds::Read(s);
-	//printf(Birds::Read(s));
+	Birds::Read(s); // we're not interested in the request, but flush the input anyway.
 	Birds::Write(s,"200 HTTP/1.1 ok\r\n",0);
 	Birds::Write(s,"Content-Type: multipart/x-mixed-replace; boundary=mjpegstream\r\nPragma: no-cache\r\n\r\n",0);
 
@@ -40,7 +53,6 @@ void client(int s, Mat & frame)
 		sprintf(head,"--mjpegstream\r\nContent-Type: image/jpeg\r\nContent-Length: %lu\r\n\r\n",outlen);
 		Birds::Write(s,head,0);
 		int n2 = Birds::Write(s,(char*)(&outbuf[0]),outlen);
-		//printf("%4x %d %d\n",s,outlen,n2 );
 		if ( n2 < outlen )
 			break;
 
@@ -51,34 +63,64 @@ void client(int s, Mat & frame)
 
 
 int count = 0;
+
+class ClientThread : public w32::Thread
+{
+	int s;
+	cv::Mat & frame;
+public:
+	ClientThread(int s, cv::Mat & frame) 
+		: s(s), frame(frame) 
+	{
+		 count ++;
+		 printf("+ %2d " __FUNCTION__ "\t%x\n",count, this);
+	}
+	~ClientThread() 
+	{
+		count --;
+		printf("- %2d " __FUNCTION__ "\t%x\n",count, this);
+	}
+	virtual void run()
+	{
+		client(s, frame);
+		delete this;
+	}
+};
+
+
+
+class CaptureThread :  public w32::Thread
+{
+	VideoCapture & cap;
+	Mat & frame;
+public:
+	CaptureThread(VideoCapture &cap, Mat & frame )
+		: cap(cap), frame(frame) { }
+	~CaptureThread() {}
+	virtual void run()
+	{
+		bool ok = cap.open(0);
+		while ( ok )
+		{
+			g_critter.lock();
+				cap >> frame;
+			g_critter.unlock();
+
+			Sleep(30);
+		}
+		delete this;
+	}
+};
+
+
+
 int main()
 {
 	VideoCapture cap;
 	Mat frame;
 
-	class CaptureThread :  public w32::Thread
-	{
-		VideoCapture & cap;
-		Mat & frame;
-	public:
-		CaptureThread(VideoCapture &cap, Mat & frame )
-			: cap(cap), frame(frame) { }
-		~CaptureThread() {}
-		virtual void run()
-		{
-			bool ok = cap.open(0);
-			while ( ok )
-			{
-				g_critter.lock();
-					cap >> frame;
-				g_critter.unlock();
-
-				Sleep(30);
-			}
-			delete this;
-		}
-	} * ct = new CaptureThread(cap,frame);
-	ct->start();
+	CaptureThread ct(cap,frame);
+	ct.start();
 
 	int serv = Birds::Server(7777);
 	while(1) 
@@ -87,29 +129,7 @@ int main()
 		if ( c < 0 ) 
 			break;
 
-		class ClientThread : public w32::Thread
-		{
-			int s;
-			cv::Mat & frame;
-		public:
-			ClientThread(int s, cv::Mat & frame) 
-				: s(s), frame(frame) 
-			{
-				 count ++;
-				 printf("+ %2d " __FUNCTION__ "\t%x\n",count, this);
-			}
-			~ClientThread() 
-			{
-				count --;
-				printf("- %2d " __FUNCTION__ "\t%x\n",count, this);
-			}
-			virtual void run()
-			{
-				client(s, frame);
-				Thread::stop();
-				delete this;
-			}
-		} * cl = new ClientThread(c,frame);
+		ClientThread * cl = new ClientThread(c,frame);
 		cl->start();
 	}
 	Birds::Close(serv);
